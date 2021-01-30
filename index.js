19d71b2d5044920a9885609ddb93e9b54ea87aa2
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-01-30 20:11:15 -0300

    Mechanism adapted to write from primary context object Reading content from the database file

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ function store(key, value) {
 }
 assert.deepEqual(store.call({ root: {} }, 'foo', 'bar'), { 'foo': 'bar' }, 'Store engine structure');
 
-function Engine() {
+function Engine(opts) {
+  const self = this;
   this.root = {};
+
+  if (typeof(opts) === 'object') {
+    Object.keys(opts).map(k => self[k] = opts[k]);
+  }
 }
 assert.ok((new Engine()) instanceof Engine, 'Engine instantiation');
 Engine.prototype.store = function() { return store.apply(this, Array.from(arguments)); }
@@ -29,10 +34,18 @@ function write(filePath='./engine.db') {
   fs.writeFileSync(filePath, data);
   return data;
 }
-assert.deepEqual(write.call({root: { 'foo': 'bar' }}), JSON.stringify({ 'foo': 'bar' }));
+assert.deepEqual(write.call({ root: { 'foo': 'bar' } }), JSON.stringify({ 'foo': 'bar' }));
 assert.ok(fs.existsSync('./engine.db'));
 
-Engine.prototype.write = function() { return write.apply(this, Array.from(arguments)); }
+Engine.prototype.write = function() {
+  let args = [];
+  let _cachedArgs = Array.from(arguments);
+  if (this.dbPath !== undefined) {
+    args.push(this.dbPath);
+  }
+  _cachedArgs.map(v => args.push(v));
+  return write.apply(this, args);
+}
 assert.deepEqual((new Engine()).write(), JSON.stringify({ 'foo': 'bar' }));
 
 assert.ok((new Engine()).root !== undefined);
@@ -43,4 +56,21 @@ assert.deepEqual(engine.root, { 'foo': 'bar' });
 engine.write();
 assert.deepEqual(JSON.parse(fs.readFileSync('./engine.db')), { 'foo': 'bar' });
 
+function read() {
+  return JSON.parse(fs.readFileSync(this.dbPath));
+}
+
+let sampleRoot = {};
+store.call({ root: sampleRoot }, 'foo', 'baz');
+write.call({ root: sampleRoot }, './custom-engine.db');
+assert.deepEqual(read.call({ dbPath: './custom-engine.db' }), { 'foo': 'baz' });
+
+Engine.prototype.read = function () { return read.apply(this, Array.from(arguments)); }
+
+let customEngine = new Engine({ dbPath: './custom-engine.db' });
+customEngine.store('foo', 'baz');
+customEngine.write();
+assert.deepEqual(customEngine.read(), { 'foo': 'baz' });
+
+fs.unlinkSync('./custom-engine.db');
 fs.unlinkSync('./engine.db');
