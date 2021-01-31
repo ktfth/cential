207d3274b1455bf2bc90b250194d54ee65f6e8e6
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-01-31 15:08:01 -0300

    Exposed all the api for test

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function Engine(opts) {
 }
 exports.Engine = Engine;
 Engine.prototype.store = function() { return store.apply(this, Array.from(arguments)); };
-assert.deepEqual((new Engine()).store('foo', 'bar'), { 'foo': 'bar' });
 
 function write(filePath='./engine.db') {
   let data = null;
@@ -34,8 +33,7 @@ function write(filePath='./engine.db') {
   fs.writeFileSync(filePath, data);
   return data;
 }
-assert.deepEqual(write.call({ root: { 'foo': 'bar' } }), JSON.stringify({ 'foo': 'bar' }));
-assert.ok(fs.existsSync('./engine.db'));
+exports.write = write;
 
 Engine.prototype.write = function() {
   let args = [];
@@ -46,56 +44,25 @@ Engine.prototype.write = function() {
   _cachedArgs.map(v => args.push(v));
   return write.apply(this, args);
 };
-assert.deepEqual((new Engine()).write(), JSON.stringify({ 'foo': 'bar' }));
-
-assert.ok((new Engine()).root !== undefined);
-
-let engine = new Engine();
-engine.store('foo', 'bar');
-assert.deepEqual(engine.root, { 'foo': 'bar' });
-engine.write();
-assert.deepEqual(JSON.parse(fs.readFileSync('./engine.db')), { 'foo': 'bar' });
 
 function read() {
   return JSON.parse(fs.readFileSync(this.dbPath));
 }
-
-let sampleRoot = {};
-store.call({ root: sampleRoot }, 'foo', 'baz');
-write.call({ root: sampleRoot }, './custom-engine.db');
-assert.deepEqual(read.call({ dbPath: './custom-engine.db' }), { 'foo': 'baz' });
+exports.read = read;
 
 Engine.prototype.read = function () { return read.apply(this, Array.from(arguments)); };
-
-let customEngine = new Engine({ dbPath: './custom-engine.db' });
-customEngine.store('foo', 'baz');
-customEngine.write();
-assert.deepEqual(customEngine.read(), { 'foo': 'baz' });
 
 function unstore(key) {
   delete this.root[key];
   return this.root;
 }
-
-store.call({ root: sampleRoot }, 'bar', 'biz');
-unstore.call({ root: sampleRoot }, 'bar');
-assert.deepEqual(sampleRoot, { 'foo': 'baz' });
+exports.unstore = unstore;
 
 Engine.prototype.unstore = function () { return unstore.apply(this, Array.from(arguments)); };
-
-customEngine.store('bar', 'biz');
-customEngine.unstore('bar');
-assert.deepEqual(customEngine.root, { 'foo': 'baz' });
 
 function get(key) {
   return this.root[key];
 }
-
-assert.equal(get.call({ root: sampleRoot }, 'foo'), 'baz');
+exports.get = get;
 
 Engine.prototype.get = function () { return get.apply(this, Array.from(arguments)) };
-
-assert.equal(customEngine.get('foo'), 'baz');
-
-fs.unlinkSync('./custom-engine.db');
-fs.unlinkSync('./engine.db');
