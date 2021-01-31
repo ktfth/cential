fc7f2d6886fdb188947ded2e3aa81dacb59f919f
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-01-31 15:08:11 -0300

    Tests migrated

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,86 @@
 'use strict';
+const fs = require('fs');
 const assert = require('assert');
 
-const { store, Engine } = require('../');
+const { Engine, store, unstore, write, read, get } = require('../');
 
 describe('Engine', () => {
+  after(() => {
+    fs.unlinkSync('./custom-engine.db');
+    fs.unlinkSync('./engine.db');
+  });
+
   it('should store data', () => {
     assert.deepEqual(store.call({ root: {} }, 'foo', 'bar'), { 'foo': 'bar' }, 'Store engine structure');
   });
 
   it('should setup the instance', () => {
     assert.ok((new Engine()) instanceof Engine, 'Engine instantiation');
+  });
+
+  it('should store data by instance', () => {
+    assert.deepEqual((new Engine()).store('foo', 'bar'), { 'foo': 'bar' });
+  });
+
+  it('should write data', () => {
+    assert.deepEqual(write.call({ root: { 'foo': 'bar' } }), JSON.stringify({ 'foo': 'bar' }));
+  });
+
+  it('should check file existance', () => {
+    assert.ok(fs.existsSync('./engine.db'));
+  });
+
+  it('should write data by instance', () => {
+    assert.deepEqual((new Engine()).write(), JSON.stringify({ 'foo': 'bar' }));
+  });
+
+  it('should instance have root', () => {
+    assert.ok((new Engine()).root !== undefined);
+  });
+
+  it('should have engine.db file created', () => {
+    let engine = new Engine();
+    engine.store('foo', 'bar');
+    assert.deepEqual(engine.root, { 'foo': 'bar' });
+    engine.write();
+    assert.deepEqual(JSON.parse(fs.readFileSync('./engine.db')), { 'foo': 'bar' });
+  });
+
+  let sampleRoot;
+
+  it('should read data', () => {
+    sampleRoot = {};
+    store.call({ root: sampleRoot }, 'foo', 'baz');
+    write.call({ root: sampleRoot }, './custom-engine.db');
+    assert.deepEqual(read.call({ dbPath: './custom-engine.db' }), { 'foo': 'baz' });
+  });
+
+  let customEngine;
+
+  it('should read data by instance', () => {
+    customEngine = new Engine({ dbPath: './custom-engine.db' });
+    customEngine.store('foo', 'baz');
+    customEngine.write();
+    assert.deepEqual(customEngine.read(), { 'foo': 'baz' });
+  });
+
+  it('should unstore data', () => {
+    store.call({ root: sampleRoot }, 'bar', 'biz');
+    unstore.call({ root: sampleRoot }, 'bar');
+    assert.deepEqual(sampleRoot, { 'foo': 'baz' });
+  });
+
+  it('should unstore data by instance', () => {
+    customEngine.store('bar', 'biz');
+    customEngine.unstore('bar');
+    assert.deepEqual(customEngine.root, { 'foo': 'baz' });
+  });
+
+  it('should get data', () => {
+    assert.equal(get.call({ root: sampleRoot }, 'foo'), 'baz');
+  });
+
+  it('should get data by instance', () => {
+    assert.equal(customEngine.get('foo'), 'baz');
   });
 });
