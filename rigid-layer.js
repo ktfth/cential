8399f85d24f4314c91a9ccabdd5f03a78724762b
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-02-03 13:49:08 -0300

    Saving data

--- rigid-layer.js
+++ rigid-layer.js
@@ -1,12 +1,19 @@
 'use strict';
+const fs = require('fs');
 
-function RigidLayer() {
+function RigidLayer(opts={}) {
+  this.filename = opts.filename || './rigid-layer.db';
   this.grid = [];
 }
 
 RigidLayer.prototype.push = function () {
   let args = Array.from(arguments);
   args.forEach(v => this.grid.push(v));
+  return this;
+};
+
+RigidLayer.prototype.save = function () {
+  fs.writeFileSync(this.filename, JSON.stringify(this.grid));
   return this;
 };
 
