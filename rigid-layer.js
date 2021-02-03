1a5db2e1b1910f29d922e8c8c735f1bdbb9fc838
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-02-03 14:00:47 -0300

    Checkpoint operation to maintain a progress of the usage on disc

--- rigid-layer.js
+++ rigid-layer.js
@@ -4,11 +4,13 @@ const fs = require('fs');
 function RigidLayer(opts={}) {
   this.filename = opts.filename || './rigid-layer.db';
   this.grid = [];
+  this.cursor = 0;
 }
 
 RigidLayer.prototype.push = function () {
   let args = Array.from(arguments);
   args.forEach(v => this.grid.push(v));
+  this.cursor += 1;
   return this;
 };
 
@@ -19,6 +21,14 @@ RigidLayer.prototype.save = function () {
 
 RigidLayer.prototype.load = function () {
   this.grid = JSON.parse(fs.readFileSync(this.filename));
+  return this;
+};
+
+RigidLayer.prototype.checkpoint = function () {
+  fs.writeFileSync(this.filename.replace('.db', '.kpt'), JSON.stringify({
+    cursor: this.cursor,
+    blocks: this.grid.filter(v => typeof v === 'object').length,
+  }));
   return this;
 };
 
