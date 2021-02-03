c121095a3355a7022939145ce66bc28ed6ba5ab5
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-02-03 13:22:35 -0300

    Rigid layer push data to the grid

--- rigid-layer.js
+++ rigid-layer.js
@@ -4,4 +4,10 @@ function RigidLayer() {
   this.grid = [];
 }
 
+RigidLayer.prototype.push = function () {
+  let args = Array.from(arguments);
+  args.forEach(v => this.grid.push(v));
+  return this;
+};
+
 module.exports = RigidLayer;
